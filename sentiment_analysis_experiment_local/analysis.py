79f7af33848b2ec0e05db1f00e46b1ce9b0d75d1
WillhelmKai <Willhelmkai@outlook.com>
2019-10-08 15:39:10 +0800

    Update analysis.py

--- sentiment_analysis_experiment_local/analysis.py
+++ sentiment_analysis_experiment_local/analysis.py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 from bert_embedding import BertEmbedding
 from collections import Counter
+import preprocessor as p
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
 
 
 add = "C:\\Users\\willh\\Documents\\Big Data\\Project\\sentiment140\\sentiment140.csv"
@@ -24,18 +27,23 @@ content = data['Content']
 #print(content.head())
 
 for i in range(0, len(content)):
+  print("    ")
   sentence = content[i]
   print(sentence)
+  print(len(sentence))
+  sentence = p.clean(sentence).split('\n')
+  print("    ")
+  print(sentence)
+
 #pre-process
 
-
-#word vectorlization
+#word vectorlization/ tokenlize
   result = bert_embedding(sentence)
-  print(result[1][1][0].shape)
-  # print(result[1][1][0])
-  # vector[i] = np.array([result[1][1][0]])
-  vector += [result[1][1][0]]
 
+  # print(len(result[0][1]))
+  # print(np.shape(np.array(result[0][1])))
+  vector += [np.array(result[0][1])]
+  # break
 #merge
 data['Vector'] = vector
 
