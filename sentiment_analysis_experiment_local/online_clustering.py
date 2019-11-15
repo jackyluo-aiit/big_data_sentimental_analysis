84261ab09d4400fda4eb522fe64099f79280976f
jackyluo <k530003036@mail.uic.edu.hk>
2019-11-15 16:49:16 +0800

    update online-clustering

--- sentiment_analysis_experiment_local/online_clustering.py
+++ sentiment_analysis_experiment_local/online_clustering.py
@@ -17,15 +17,15 @@ from sentiment_analysis_experiment_local.clean_data import *
 
 class online_kmeans_pipeline(object):
     def __init__(self):
-        self.cluster_pd = pd.DataFrame(columns=['index', 'label'])
+        self.cluster_pd = pd.DataFrame(columns=['label'])
         self.mean_centroids = np.load('mean_centroid.npy')
-        modelfile = '/Users/jackyluo/OneDrive - The Chinese University of Hong Kong/Big ' \
-                    'Data/project/the-disagreeable-frogs/fasttext_model/twitter_unigrams.bin'
+        modelfile = 'twitter_unigrams.bin'
         if os.path.exists(modelfile):
             self.model = sent2vec.Sent2vecModel()
             self.model.load_model(modelfile)
+            print('...model loaded!...')
         else:
-            print("Model not found!")
+            print("...Model not found!...")
 
     def distCosin(self, vecA, vecB):
         vecA = np.mat(vecA)
@@ -33,15 +33,15 @@ class online_kmeans_pipeline(object):
         dist = spatial.distance.cosine(vecA, vecB)
         return dist
 
-    def updataMena(self, vec, mean, n):
+    def updateMean(self, vec, mean, n):
         new_mean = mean + (vec - mean) * (1 / n)
         return new_mean
 
     def preprocess_content(self, raw_content):
         line = raw_content
-        line = p.clean(line)
         # print(line)
         if isinstance(line, str):
+            line = p.clean(line)
             clean_line = clean_tweet(line)
             clean_line = clean_punc(clean_line)
             clean_line = wordlemmatize(clean_line)
@@ -58,9 +58,9 @@ class online_kmeans_pipeline(object):
             filtered_words = [word for word in words if word not in stopwords.words('english')]
             preprocessed_content = " ".join(str(x) for x in filtered_words)
             preprocessed_content = preprocessed_content
-            print("after preprocessed:", preprocessed_content)
             return preprocessed_content
         else:
+            print(line, ': It is not a string!')
             return -1
 
     def embed_content(self, preprocessed_content):
@@ -70,15 +70,14 @@ class online_kmeans_pipeline(object):
             emb = self.model.embed_sentence(preprocessed_content)
             # print(emb)
             vectorized_content = np.array(emb)
-            print(np.shape(vectorized_content))
-            with open("vectorized.npy", 'a+') as f:
-                # f = file("vectorized.npy", 'a')
-                np.save("vectorized.npy", vectorized_content)
-                f.close()
-                print("save to file: ", 'vectorized.npy')
+            #             print('embedded vector:', np.shape(vectorized_content))
+            #             with open("vectorized.npy", 'a+') as f:
+            #                 # f = file("vectorized.npy", 'a')
+            #                 np.save("vectorized.npy", vectorized_content)
+            #                 f.close()
+            #                 print("save to file: ", 'vectorized.npy')
             return vectorized_content
         else:
-            print('It is not a string!')
             return -1
 
     def kmeanspp(self, vectorized_content, in_index):
@@ -87,35 +86,38 @@ class online_kmeans_pipeline(object):
         po_mean = np.copy(self.mean_centroids[1, :])
         ne_mean = np.array(ne_mean)
         po_mean = np.array(po_mean)
-        print("ne_mean:", np.shape(ne_mean))
-        print("po_mean:", np.shape(po_mean))
+        #         print("ne_mean:", np.shape(ne_mean))
+        #         print("po_mean:", np.shape(po_mean))
         print("vectorized_content:", np.shape(input_vec))
         dist_ne = self.distCosin(ne_mean, input_vec)
         dist_po = self.distCosin(po_mean, input_vec)
-        self.cluster_pd.loc[in_index, 'index'] = in_index
+        tmp_cluster = pd.DataFrame(columns=['label'])
         if dist_ne <= dist_po:
             self.cluster_pd.loc[in_index, 'label'] = 0
+            tmp_cluster.loc[in_index, 'label'] = 0
             n = self.cluster_pd.loc[self.cluster_pd['label'] == 0].shape[0]
-            print('number of elements in current cluster:', n)
-            self.mean_centroids[0, :] = self.updataMena(vectorized_content, ne_mean, n)
+            print('number of elements in negative cluster:', n)
+            self.mean_centroids[0, :] = self.updateMean(vectorized_content, ne_mean, n)
         else:
             self.cluster_pd.loc[in_index, 'label'] = 1
+            tmp_cluster.loc[in_index, 'label'] = 1
             n = self.cluster_pd.loc[self.cluster_pd['label'] == 1].shape[0]
-            print('number of elements in current cluster:', n)
-            self.mean_centroids[1, :] = self.updataMena(vectorized_content, ne_mean, n)
+            print('number of elements in positive cluster:', n)
+            self.mean_centroids[1, :] = self.updateMean(vectorized_content, po_mean, n)
+        tmp_cluster.to_csv('tmp_result.csv', mode='a+', header=False)
 
     def process_content(self, raw_content):
         df = raw_content
         for index, row in df.iterrows():
             preprocessed_content = self.preprocess_content(row['content'])
-            if len(preprocessed_content.split()) >= 3:
-                vectorized_content = self.embed_content(preprocessed_content)
-                in_index = row['in_index']
-                self.kmeanspp(vectorized_content, in_index)
-            else:
-                'Less 3 words left after preprocessed!!!'
-        # self.cluster_pd.to_csv('cluster_result.csv', mode='a+', header=False)
-        return self.cluster_pd
+            if isinstance(preprocessed_content, str):
+                print("after preprocessed:", preprocessed_content)
+                if len(preprocessed_content.split()) >= 3:
+                    vectorized_content = self.embed_content(preprocessed_content)
+                    in_index = row['in_index']
+                    self.kmeanspp(vectorized_content, in_index)
+                else:
+                    'Less than 3 words left after preprocessed!!!'
 
     def saveresult(self):
         self.cluster_pd.to_csv('cluster_result.csv', mode='a+', header=False)
--- sentiment_analysis_experiment_local/test.py
+++ sentiment_analysis_experiment_local/test.py
@@ -1,6 +1,8 @@
 from sentiment_analysis_experiment_local.online_clustering import online_kmeans_pipeline
 import pandas as pd
+# pd.set_option('display.float_format', lambda x: '%.2f' % x)
 import numpy as np
+# np.set_printoptions(suppress=True)
 import csv
 
 
@@ -15,17 +17,19 @@ def computefpp(cluster_dict, original_data, result_filename, model=0):
                     key = 4
                 if original_data.loc[item, ['label']].values != key:
                     count += 1
-                cluster_fp = count / len(cluster_set)
-                cluster_fp_set[key] = cluster_fp
+            cluster_fp = count / len(cluster_set)
+            cluster_fp_set[key] = cluster_fp
         if model == 1:
             for item in cluster_set:
-                if key == 1 and original_data.loc[item, ['polarity']].values <= 0:
-                    count += 1
-                if key == 0 and original_data.loc[item, ['polarity']].values > 0:
-                    count += 1
+                if item in original_data.index:
+                    if key == 1 and original_data.loc[item, ['polarity']].values <= 0:
+                        count += 1
+                    if key == 0 and original_data.loc[item, ['polarity']].values > 0:
+                        count += 1
+                else:
+                    print("error:", item)
             cluster_fp = count / len(cluster_set)
-
-        cluster_fp_set[key] = cluster_fp
+            cluster_fp_set[key] = cluster_fp
     print("fpp:\n", cluster_fp_set)
     with open(result_filename, 'a+') as f:
         w = csv.writer(f)
@@ -34,31 +38,37 @@ def computefpp(cluster_dict, original_data, result_filename, model=0):
 
 
 data = pd.read_csv("database_data.csv", encoding="utf-8")
-data.columns = ['', '', '', '', 'content', '', 'polarity', '', '', '', '', '', '', '', '', '', '', 'label', '']
-raw_content = pd.DataFrame(columns=['in_index', 'content'])
+data.columns = ['database_id', '', '', '', 'content', '', 'polarity', '', '', '', '', '', '', '', '', '', '', 'label', '']
+data = data.set_index('database_id')
+data = data.drop(index='root')
+data.index = data.index.astype('int64')
+print(data.index.astype('int64'))
+
 okp = online_kmeans_pipeline()
 
 for index, row in data.iterrows():
     raw_content = pd.DataFrame(columns=['in_index', 'content'])
-    raw_content = raw_content.append(pd.DataFrame({'in_index': [index], 'content': [row['content']]}))
+    raw_content = raw_content.append(pd.DataFrame({'in_index': index, 'content': [row['content']]}))
     print(raw_content)
     okp.process_content(raw_content)
 
 okp.saveresult()
+
 df = pd.read_csv('cluster_result.csv')
-print(df)
+df.columns = ['index','label']
+df = df.set_index('index')
+print(df.index)
 label_dict = {}
 
 ne_list=[]
 po_list=[]
 for index, row in df.iterrows():
     if row['label'] == 0:
-        ne_list.append(row['in_index'])
+        ne_list.append(index)
     else:
-        po_list.append(row['in_index'])
+        po_list.append(index)
 
 label_dict[0] = ne_list
 label_dict[1] = po_list
 print(label_dict)
 computefpp(label_dict, data, "mean-result_online_clustering.csv", model=1)
-
