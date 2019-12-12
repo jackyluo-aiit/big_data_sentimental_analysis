f61ee37c63ea99a39b67de3a373fc5e7b1b93607
jackyluo-aiit <1576123082@qq.com>
2019-12-12 21:52:01 +0800

    update reducer.py

--- sentiment_analysis_experiment_local/reducer.py
+++ sentiment_analysis_experiment_local/reducer.py
@@ -1,14 +1,23 @@
 #coding by Will
 from operator import itemgetter
 import sys
+import os
+import sent2vec
 from collections import Counter 
 #hold a dictionary
+modelfile = '/home/lxq/PycharmProjects/the-disagreeable-frogs/fasttext_model/twitter_unigrams.bin'
+if os.path.exists(modelfile):
+    model = sent2vec.Sent2vecModel()
+    model.load_model(modelfile)
+    print('...model loaded!...')
+else:
+    print("...Model not found!...")
 dic = {}
 for line in sys.stdin:
   line = line.strip()
   categoriy, content = line.split('\t', 1)
   #embedding
-  comtent = embedding 
+  content = model.embed_sentence(content)
   #find current category weather existed in the dic
   if dic.get(categoriy)  == None:
     dic[categoriy] = content
